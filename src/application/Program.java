57879029b7e0aa042cdb81df6b56fbb004eed95d
Vitienzo Capeletti <vitienzodias@gmail.com>
2021-02-18 23:24:15 -0300

    Início de um projeto

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import boardGame.Board;
 import boardGame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Position pos = new Position(3, 5);
+        ChessMatch chessMatch = new ChessMatch();
+        //UserInterface
+        UI.printBoard(chessMatch.getPieces());
 
-        System.out.println(pos);
+
+
 
     }
 
