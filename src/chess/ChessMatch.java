2ace0cb7e4561abd56044c1988151a3b61b82e06
Vitienzo Capeletti <vitienzodias@gmail.com>
2021-02-18 23:28:00 -0300

    Atualização projeto 1

---
+++ src/chess/ChessMatch.java
@@ -0,0 +1,37 @@
+package chess;
+
+import boardGame.Board;
+import boardGame.Position;
+import pieces.King;
+import pieces.Rook;
+
+public class ChessMatch {
+
+    private Board board;
+
+    public ChessMatch() {
+        board = new Board(8, 8);
+        initialSetup();
+    }
+
+    //Retornar uma matriz de peças de xadrez correspondentes a essa partida
+    public ChessPiece[][] getPieces() {
+        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
+                mat[i][j] = (ChessPiece) board.piece(i, j);
+            }
+
+        }
+
+        return mat;
+    }
+
+    private void initialSetup() {
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+
+    }
+
+}
